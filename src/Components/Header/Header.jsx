dcf56d94b98b609f808330bf5d2dbfc55d26e344
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2023-09-12 17:06:50 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import profilePhoto from "../../assets/images/profile.png"
+const Header = () => {
+  return (
+    <div>
+      <p>Knowledge Cafe</p>
+      <img src={profilePhoto} alt="" />
+    </div>
+  );
+};
+
+export default Header;
